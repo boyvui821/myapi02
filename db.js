5f571a4dc16d72d113b0b4e57dcfb9302feec5c9
boyvui821 <6018821+boyvui821@users.noreply.github.com>
2019-06-26 15:32:13 +0700

    Create db.js

---
+++ db.js
@@ -0,0 +1,24 @@
+{
+  "loans": [
+        {
+          "amount": 111222333,
+          "title": "Vay cưới vợ",
+          "rate": 0.45
+        },
+        {
+          "amount": 45000000,
+          "title": "Vay mua đảo",
+          "rate": 0.45
+        },
+        {
+          "amount": 13000000,
+          "title": "Vay mua điện thoại",
+          "rate": 0.45
+        },
+        {
+          "amount": 34000000,
+          "title": "Vay mua laptop",
+          "rate": 0.45
+        },
+      ]
+}
